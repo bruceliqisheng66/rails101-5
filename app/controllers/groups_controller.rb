322bbd2913895a7bd4afb213a7d1acf781c5b8fa
li qi sheng <bruce.liqisheng66@gmail.com>
2016-12-21 11:34:35 +0800

    implement group#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,10 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def create
    @group = Group.new(group_params)
    @group.save
@@ -20,5 +24,5 @@ private
 def group_params
   params.require(:group).permit(:title, :description)
 end
-  
+
 end
